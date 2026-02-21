1be3dac2f3f434be117c1b6a342efd01e2f1dd34
Misha Volkov <mike.volk107@yandex.ru>
2026-02-21 15:41:42 +0300

    was writing tests and added coverage for unit tests

---
+++ tests/test_burger.py
@@ -0,0 +1,49 @@
+BUN_PRICE = 50
+BUN_NAME_1 = "Test Bun 1"
+BUN_NAME_2 = "Test Bun 2"
+SAUCE_NAME = "Test Sauce"
+FILLING_NAME = "Test Filling"
+SAUCE_PRICE = 20
+FILLING_PRICE = 40
+
+def test_add_one_ingredient(burger, mock_sauce):
+    burger.add_ingredient(mock_sauce)
+    assert burger.ingredients[0].get_name() == SAUCE_NAME, "Ингредиент не был добавлен корректно"
+
+def test_add_two_ingredients(burger, mock_sauce, mock_filling):
+    burger.add_ingredient(mock_sauce)
+    burger.add_ingredient(mock_filling)
+    assert burger.ingredients[0].get_name() == SAUCE_NAME and burger.ingredients[
+        1].get_name() == FILLING_NAME, "Ингредиенты не были добавлены корректно"
+
+def test_remove_ingredient(burger, mock_sauce, mock_filling):
+    burger.add_ingredient(mock_sauce)
+    burger.add_ingredient(mock_filling)
+    burger.remove_ingredient(1)
+    assert burger.ingredients[0].get_name() == SAUCE_NAME and len(
+        burger.ingredients) == 1, "Ингредиент не был удален корректно"
+
+def test_move_ingredient(burger, mock_sauce, mock_filling):
+    burger.add_ingredient(mock_sauce)
+    burger.add_ingredient(mock_filling)
+    burger.move_ingredient(0, 1)
+    assert burger.ingredients[0].get_name() == FILLING_NAME and burger.ingredients[
+        1].get_name() == SAUCE_NAME, "Ингредиент не был перемещен корректно"
+
+def test_get_price(burger, mock_bun, mock_sauce, mock_filling):
+    burger.set_buns(mock_bun)
+    burger.add_ingredient(mock_sauce)
+    burger.add_ingredient(mock_filling)
+    expected_price = BUN_PRICE * 2 + SAUCE_PRICE + FILLING_PRICE
+    assert burger.get_price() == expected_price, f"Общая цена бургера {burger.get_price()} не соответствует ожидаемой {expected_price}"
+
+def test_get_receipt(burger, mock_bun, mock_sauce):
+    burger.set_buns(mock_bun)
+    burger.add_ingredient(mock_sauce)
+    expected_price = BUN_PRICE * 2 + SAUCE_PRICE
+    receipt = burger.get_receipt()
+    assert all([
+        BUN_NAME_1 in receipt,
+        SAUCE_NAME in receipt,
+        f"Price: {expected_price}" in receipt
+    ]), "Чек сформирован неверно"
